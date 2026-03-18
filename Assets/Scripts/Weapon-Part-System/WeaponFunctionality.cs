c08ccbfbc3ec159ef2c999ad6ed26768650fb2c1
Stanley Buenaventura <105530737+SBSonk@users.noreply.github.com>
2026-03-18 17:46:49 +0800

    Apply some weapon stats from scriptable object, bullet visual rework, projectile framework

--- Assets/Scripts/Weapon-Part-System/WeaponFunctionality.cs
+++ Assets/Scripts/Weapon-Part-System/WeaponFunctionality.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class FunctionalWeapon : PartTemplate
 {
+    protected Part weaponData; 
+
+    public virtual void InitializeWeapon(Part data)
+    {
+        weaponData = data;
+    }
+
     public virtual void OnFireHeld() {}
     public virtual void OnFirePressed() {}
     public virtual void OnFireReleased() {}
