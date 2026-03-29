7880c48e6356f377116dd7bf0323049c4850c37a
Dart <mesiamatthew@gmail.com>
2026-03-30 00:01:47 +0800

    added sfx to assets

--- Assets/ScriptableObjects/EnemyData/EnemyDataSO.cs
+++ Assets/ScriptableObjects/EnemyData/EnemyDataSO.cs
@@ -6,8 +6,6 @@ public class EnemyDataSO : ScriptableObject
     public enum EnemyRank { D, C, B, A, S, SS, SSS }
 
     public string enemyName;
-    public int health;
-    public int damage;
     public string description;
     public GameObject enemyPrefab;
     public EnemyRank rank;
